ce56bdc959adab5db78aeea7cb33757635dd07a9
SiddharthM2416 <sid24.malik@gmail.com>
2025-11-14 18:17:01 +0530

    inital backend commit

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+require('dotenv').config();
+const connectDB = async () => {
+    try{
+        await mongoose.connect(process.env.MONGOOSE)
+        console.log("DB Connected")
+    } catch(error) {
+        console.log("DB connection faile : ",error.message);
+        process.exit(1)
+    }
+}
+
+module.exports = {connectDB}
---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const cors = require("cors")
+const { connectDB } = require("./config/db.js")
+const foodRouter = require("./routes/foodRoute.js")
+
+
+// app config
+const app = express()
+const port = 4000
+
+// middleware
+app.use(express.json())
+app.use(cors())
+
+// db connection
+connectDB();
+
+// api endpoint
+app.use("/api/food",foodRouter)
+
+app.get("/",(req,res)=>{
+    res.send("API Working")
+})
+
+app.listen(port,()=>{
+    console.log(`Server Started on http://localhost:${port}`)
+})
+
