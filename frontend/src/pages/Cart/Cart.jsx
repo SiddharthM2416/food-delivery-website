d7ac63bc3f6d26f6593027a44c12f3b730eb2d5b
SiddharthM2416 <sid24.malik@gmail.com>
2025-11-21 14:11:24 +0530

    Minor fixes

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -12,8 +12,7 @@ const Cart = () => {
     <div className='cart'>
       <div className="cart-items">
         <div className="cart-items-title">
-          <p>Items</p>
-          <p>Title</p>
+          <p>Item</p>
           <p>Price</p>
           <p>Quantity</p>
           <p>Total</p>
@@ -27,7 +26,6 @@ const Cart = () => {
             return(
               <div>
               <div className='cart-items-title cart-items-item'>
-                <img src={item.image} alt="food" />
                 <p>{item.name}</p>
                 <p>${item.price}</p>
                 <p>{cartItems[item._id]}</p>
